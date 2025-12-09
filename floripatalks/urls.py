50878bf7758d78b377fd93bde5fdb82ceeb91900
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2025-12-09 20:00:09 -0300

    fix: Prefix unused request parameter with underscore

--- floripatalks/urls.py
+++ floripatalks/urls.py
@@ -3,9 +3,19 @@ URL configuration for floripatalks project.
 """
 
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
 from django.urls import include, path
 
+
+def home(_request: HttpRequest) -> HttpResponse:
+    """Temporary home view for testing."""
+    return HttpResponse(
+        "<h1>FloripaTalks</h1><p>Servidor Django funcionando!</p><p><a href='/admin/'>Admin</a></p>"
+    )
+
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("events/", include("events.urls")),
     path("accounts/", include("accounts.urls")),
